36cffec77ccd80f53d04b6a18566bb9ce59a792e
Manuel Reyes <Manueldjsr@gmail.com>
2024-02-28 21:56:00 -0400

    Update user.test.ts

--- src/repositories/user-repository/__tests__/user.test.ts
+++ src/repositories/user-repository/__tests__/user.test.ts
@@ -5,10 +5,14 @@ import request from "supertest";
 describe("POST /api/users create user", () => {
   it("should create a user and return 200 status", async () => {
     const payload: Record<string, string> = {
-      username: "Gon",
-      password: "gon",
-      email: "gon@gmail.com"
+      username: "Gonel",
+      password: "gonel",
+      email: "gone@gmail.com"
     }
+
+    // const expectedBodyResponse = {
+    //   message: "User created successfully"
+    // }
 
     const res = await request(app)
       .post("/api/users")
@@ -16,7 +20,14 @@ describe("POST /api/users create user", () => {
       .set('Content-Type', 'application/json')
       .set('Accept', 'application/json')
 
+
       expect(res.statusCode).toEqual(200);
+
+      /* 
+        - Make sure to create test for body response
+      */
+      // expect(res.body).toEqual(expectedBodyResponse);  
+      
   });
 
 });
